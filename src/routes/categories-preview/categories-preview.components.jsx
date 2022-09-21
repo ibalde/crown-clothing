9fba238f3a26cd62a54e098a56104990acef05ec
Ibrahime Balde <ibrahimebalde.dev@gmail.com>
2022-09-21 17:26:02 -0400

    Pull data from firebase and adding routing

---
+++ src/routes/categories-preview/categories-preview.components.jsx
@@ -0,0 +1,19 @@
+import { useContext, Fragment } from 'react';
+import { CategoriesContext } from '../../context/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.components';
+
+const CategoriesPreview = ()=>{
+    var {categoriesMap} = useContext(CategoriesContext);
+    return (
+        <Fragment>
+            {
+                Object.keys(categoriesMap).map((title) => 
+                    <CategoryPreview key={title} title={title} products={categoriesMap[title]}/>
+                )
+            }
+        </Fragment>
+        
+    )
+}
+
+export default CategoriesPreview;
